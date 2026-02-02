72069845b03c211cdf84e3d7d322b47cae93605e
Andrey <andriealohin2020@outlook.com>
2026-02-03 00:13:53 +0200

    start from beginning

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import pluginJS from "@eslint/js";
+
+export default defineConfig([
+  pluginJS.configs.recommended,
+  { 
+    files: ["src/**?*js"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: "error",
+      'no-unused-vars': ['error', {args: 'none'}],
+      'no-undef': 'error'
+    },
+  },
+]);
