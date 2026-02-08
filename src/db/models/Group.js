868c717e434e74f108fc45320d71003a29ec4ea3
Andrey <andriealohin2020@outlook.com>
2026-02-08 16:06:02 +0200

    add Group model | fix User model

---
+++ src/db/models/Group.js
@@ -0,0 +1,11 @@
+import { model, Schema } from 'mongoose';
+
+const groupShcema = new Schema(
+  {
+    name: { type: String, required: true },
+    managerId: { type: Schema.ObjectId, ref: 'User', required: true },
+  },
+  { timestamps: true, versionKey: false },
+);
+
+export const Group = model('Group', groupShcema);
--- src/db/models/User.js
+++ src/db/models/User.js
@@ -1,17 +1,25 @@
 import { Schema } from 'mongoose';
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true, select: false },
-  avatarUrl: { type: String, default: undefined },
-  role: { type: String, enum: ['employee', 'manager'], default: 'employee' },
-});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true, select: false },
+    role: { type: String, enum: ['user', 'manager'], default: 'user' },
+    avatarUrl: { type: String },
+    groupId: {
+      type: Schema.ObjectId,
+      ref: 'Group',
+    },
+  },
+  { timestamps: true, versionKey: false },
+);
 
 userSchema.set('toJSON', {
   transform: (_, ret) => {
-    delete ret.password;
-    delete ret.__v;
+    delete ret.passwordHash;
     return ret;
   },
 });
+
+export const User = model('User', userSchema);
